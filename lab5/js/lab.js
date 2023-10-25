3fda9faa4639a2d83be0bbb734b0c72270a8e939
jaabarri <jaabarri1112@gmail.com>
2023-10-25 15:14:41 -0700

    Edits
    Co-authored-by: lphilbro <lphilbro@users.noreply.github.com>

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Authors:    Leyton and Jacob
+ * Created:   10.25.2023
+ * 
+ **/
 
-// Constants
+// Variable definitions
+make = "Toyota";
+model = "Camry";
+year = 2020;
+color = "blue";
 
-// Functions
+//Age calculation
+age  = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age + "<br>");
